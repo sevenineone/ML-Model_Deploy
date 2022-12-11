5217fb4e3a064112cbf1c79c6c6acd66093de9fd
sevenineone <alexeyka791@gmail.com>
2022-12-11 23:22:21 +0300

    #2 training the model

--- train.py
+++ train.py
@@ -22,3 +22,34 @@ X_scaled = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=2)
+
+model = Model(X_train.shape[1])
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+
+
+EPOCHS = 100
+X_train = Variable(torch.from_numpy(X_train)).float()
+y_train = Variable(torch.from_numpy(y_train)).long()
+X_test = Variable(torch.from_numpy(X_test)).float()
+y_test = Variable(torch.from_numpy(y_test)).long()
+
+loss_list = np.zeros((EPOCHS,))
+accuracy_list = np.zeros((EPOCHS,))
+
+for epoch in tqdm.trange(EPOCHS):
+    y_pred = model(X_train)
+    loss = loss_fn(y_pred, y_train)
+    loss_list[epoch] = loss.item()
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    with torch.no_grad():
+        y_pred = model(X_test)
+        correct = (torch.argmax(y_pred, dim=1) ==
+                   y_test).type(torch.FloatTensor)
+        accuracy_list[epoch] = correct.mean()
+
+joblib.dump(model, "model/ff_model.pkl")
